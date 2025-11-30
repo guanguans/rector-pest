9b14d5050b9317808b1311f75394514d8616beb6
Punyapal Shah <mrpunyapal@gmail.com>
2025-11-30 22:30:42 +0530

    chore: add new rules to pest-code-quality set
    Register all new code quality rules in the pest-code-quality set:
    - SimplifyToLiteralBooleanRector
    - UseTypeMatchersRector
    - UseToHaveCountRector
    - UseInstanceOfMatcherRector
    - SimplifyComparisonExpectationsRector
    - UseToMatchRector

--- config/sets/pest-code-quality.php
+++ config/sets/pest-code-quality.php
@@ -3,9 +3,15 @@
 declare(strict_types=1);
 
 use MrPunyapal\RectorPest\Rules\ChainExpectCallsRector;
+use MrPunyapal\RectorPest\Rules\SimplifyComparisonExpectationsRector;
 use MrPunyapal\RectorPest\Rules\SimplifyExpectNotRector;
+use MrPunyapal\RectorPest\Rules\SimplifyToLiteralBooleanRector;
 use MrPunyapal\RectorPest\Rules\ToBeTrueNotFalseRector;
 use MrPunyapal\RectorPest\Rules\UseEachModifierRector;
+use MrPunyapal\RectorPest\Rules\UseInstanceOfMatcherRector;
+use MrPunyapal\RectorPest\Rules\UseToHaveCountRector;
+use MrPunyapal\RectorPest\Rules\UseToMatchRector;
+use MrPunyapal\RectorPest\Rules\UseTypeMatchersRector;
 use Rector\Config\RectorConfig;
 
 /**
@@ -16,6 +22,7 @@ use Rector\Config\RectorConfig;
  * - Removing redundant code in tests
  * - Using more expressive Pest APIs
  * - Simplifying expect chains
+ * - Using dedicated matchers instead of generic comparisons
  */
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../config.php');
@@ -27,4 +34,16 @@ return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->rule(ToBeTrueNotFalseRector::class);
 
     $rectorConfig->rule(UseEachModifierRector::class);
+
+    $rectorConfig->rule(SimplifyToLiteralBooleanRector::class);
+
+    $rectorConfig->rule(UseTypeMatchersRector::class);
+
+    $rectorConfig->rule(UseToHaveCountRector::class);
+
+    $rectorConfig->rule(UseInstanceOfMatcherRector::class);
+
+    $rectorConfig->rule(SimplifyComparisonExpectationsRector::class);
+
+    $rectorConfig->rule(UseToMatchRector::class);
 };
