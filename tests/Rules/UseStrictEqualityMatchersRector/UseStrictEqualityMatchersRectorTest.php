f7ecc023889faea04279aeb773d818b4f4ce1baf
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-10 20:08:08 +0530

    refactor(tests): simplify test structure using Pest syntax and beforeEach setup

--- tests/Rules/UseStrictEqualityMatchersRector/UseStrictEqualityMatchersRectorTest.php
+++ tests/Rules/UseStrictEqualityMatchersRector/UseStrictEqualityMatchersRectorTest.php
@@ -1,28 +1,14 @@
 <?php
 
 declare(strict_types=1);
+use Rector\Testing\Fixture\FixtureFileFinder;
 
-namespace MrPunyapal\RectorPest\Tests\Rules\UseStrictEqualityMatchersRector;
+beforeEach(function () {
+    $this->configFilePath = __DIR__ . '/config/configured_rule.php';
+});
 
-use Iterator;
-use PHPUnit\Framework\Attributes\DataProvider;
-use Rector\Testing\PHPUnit\AbstractRectorTestCase;
-
-final class UseStrictEqualityMatchersRectorTest extends AbstractRectorTestCase
-{
-    #[DataProvider('provideData')]
-    public function test(string $filePath): void
-    {
-        $this->doTestFile($filePath);
-    }
-
-    public static function provideData(): Iterator
-    {
-        return self::yieldFilesFromDirectory(__DIR__ . '/Fixture');
-    }
-
-    public function provideConfigFilePath(): string
-    {
-        return __DIR__ . '/config/configured_rule.php';
-    }
-}
+test('', function (string $filePath) {
+    $this->doTestFile($filePath);
+})->with(function (): Iterator {
+    return FixtureFileFinder::yieldDirectory(__DIR__ . '/Fixture');
+});
