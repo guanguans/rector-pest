8ed4f26f8c55226bd9b40d07bc96d9dae0ac8eea
Punyapal Shah <mrpunyapal@gmail.com>
2025-12-29 17:43:22 +0530

    fix: skip toHaveKey calls with ->not modifier

--- src/Rules/UseToHaveKeysRector.php
+++ src/Rules/UseToHaveKeysRector.php
@@ -70,6 +70,11 @@ CODE_SAMPLE
         $firstCall = null;
 
         while ($current instanceof MethodCall && $this->isName($current->name, 'toHaveKey')) {
+            // Skip if any of the toHaveKey calls has a ->not modifier
+            if ($this->hasNotModifier($current)) {
+                return null;
+            }
+
             if (count($current->args) !== 1) {
                 return null;
             }
